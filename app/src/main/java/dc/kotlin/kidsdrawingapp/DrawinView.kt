f182ee5f0a654287701852412522197103d3ef40
diogo <diogo.correia01@outlook.com>
2023-09-18 14:47:31 +0100

    Logic & UI Update

--- app/src/main/java/dc/kotlin/kidsdrawingapp/DrawinView.kt
+++ app/src/main/java/dc/kotlin/kidsdrawingapp/DrawinView.kt
@@ -6,6 +6,7 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
@@ -33,6 +34,51 @@ class DrawinView(context: Context, attrs: AttributeSet) : View(context, attrs) {
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
         mBrushSize = 20.toFloat()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    // Change canvas to null if errors appeared
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+        if (!mDrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color.toArgb()
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when(event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+
+                mDrawPath!!.reset()
+                mDrawPath!!.moveTo(touchX!!, touchY!!)
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                mDrawPath!!.lineTo(touchX!!, touchY!!)
+            }
+
+            MotionEvent.ACTION_UP -> {
+                mDrawPath = CustomPath(color, mBrushSize)
+            }
+
+            else -> return false
+        }
+        invalidate()
+
+        return true
     }
 
     internal inner class CustomPath(var color: Color, var brushThickness: Float) : Path() {
