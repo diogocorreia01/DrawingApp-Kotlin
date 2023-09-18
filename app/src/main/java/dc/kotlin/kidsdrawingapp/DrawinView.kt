0dd02b9e3eee326c7f9861035e866d7787f74958
diogo <diogo.correia01@outlook.com>
2023-09-18 13:57:38 +0100

    Logic & UI Update

--- app/src/main/java/dc/kotlin/kidsdrawingapp/DrawinView.kt
+++ app/src/main/java/dc/kotlin/kidsdrawingapp/DrawinView.kt
@@ -11,12 +11,12 @@ import androidx.compose.ui.graphics.Color
 
 class DrawinView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
-    private var mDrawPath: customPath? = null
+    private var mDrawPath: CustomPath? = null
     private var mCanvasBitmap: Bitmap? = null
     private var mDrawPaint: Paint? = null
     private var mCanvasPaint: Paint? = null
     private var mBrushSize: Float = 0.toFloat()
-    private var color = Color.Black
+    private var color = Color.BLACK
     private var canvas: Canvas? = null
 
     init {
@@ -25,9 +25,16 @@ class DrawinView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
     private fun setUpDrawing() {
         mDrawPaint = Paint()
+        mDrawPath = CustomPath(color, mBrushSize)
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mBrushSize = 20.toFloat()
     }
 
-    internal inner class customPath(var color: Int, var brushThickness: Float) : Path() {
+    internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path() {
 
     }
 
