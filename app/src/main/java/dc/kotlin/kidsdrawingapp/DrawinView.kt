3d803d6afdba5595bf748df42ef324a87574048f
diogo <diogo.correia01@outlook.com>
2023-09-19 10:04:02 +0100

    Logic & UI Update

--- app/src/main/java/dc/kotlin/kidsdrawingapp/DrawinView.kt
+++ app/src/main/java/dc/kotlin/kidsdrawingapp/DrawinView.kt
@@ -20,6 +20,7 @@ class DrawinView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     private var mBrushSize: Float = 0.toFloat()
     private var color = Color.Black
     private var canvas: Canvas? = null
+    private val mPaths = ArrayList<CustomPath>()
 
     init {
         setUpDrawing()
@@ -46,6 +47,13 @@ class DrawinView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        for (path in mPaths) {
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path.color.toArgb()
+            canvas.drawPath(path, mDrawPaint!!)
+        }
+
         if (!mDrawPath!!.isEmpty) {
             mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
             mDrawPaint!!.color = mDrawPath!!.color.toArgb()
@@ -71,6 +79,7 @@ class DrawinView(context: Context, attrs: AttributeSet) : View(context, attrs) {
             }
 
             MotionEvent.ACTION_UP -> {
+                mPaths.add(mDrawPath!!)
                 mDrawPath = CustomPath(color, mBrushSize)
             }
 
